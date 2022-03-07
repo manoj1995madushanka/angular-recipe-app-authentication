9346e262f3c38c44786cf79d84c4c2c3a6bfb518
manoj1995madushanka <madushankamanoj414@gmail.com>
2022-03-07 14:31:54 +0530

    add login functionality

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -3,12 +3,13 @@ import {HttpClient} from '@angular/common/http';
 import {catchError} from 'rxjs/operators';
 import {throwError} from 'rxjs';
 
-interface AuthResponseData {
+export interface AuthResponseData {
   idToken: string,
   email: string,
   refreshToken: string,
   expiresIn: string,
-  localId: string
+  localId: string,
+  registered?: boolean
 }
 
 
@@ -22,6 +23,28 @@ export class AuthService {
 
   signup(email: string, password: string) {
     return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAu9GPEe7TPc_LroSCaX2vEdIqu01feToI'
+      , {
+        email,
+        password,
+        returnSecureToken: true
+      }).pipe(catchError(
+      error => {
+        let errorMessage = 'An unknown error occurred!';
+        // handle network errors
+        if (!error.error || !error.error.error) {
+          return throwError(errorMessage);
+        }
+        switch (error.error.error.message) {
+          case 'EMAIL_EXISTS':
+            errorMessage = 'The email address is already in use by another account.';
+        }
+        return throwError(errorMessage);
+      }
+    ));
+  }
+
+  login(email: string, password: string) {
+    return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAu9GPEe7TPc_LroSCaX2vEdIqu01feToI'
       , {
         email,
         password,
