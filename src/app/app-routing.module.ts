2e2bfb7ac313fff169b1dce661f62519c26a56c3
manoj1995madushanka <madushankamanoj414@gmail.com>
2022-03-15 23:01:31 +0530

    add auth module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,8 +10,7 @@ import { RecipesResolverService } from './recipes/recipes-resolver.service';
 import {AuthComponent} from './auth/auth.component';
 
 const appRoutes: Routes = [
-  { path: '', redirectTo: '/recipes', pathMatch: 'full' },
-  { path: 'auth', component: AuthComponent },
+  { path: '', redirectTo: '/recipes', pathMatch: 'full' }
 ];
 
 @NgModule({
