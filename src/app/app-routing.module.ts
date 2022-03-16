2c62d49d90ea26f8b8eee975fe8dde58eba4361a
manoj1995madushanka <madushankamanoj414@gmail.com>
2022-03-16 14:13:57 +0530

    auth and sopping list lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,9 @@ import {Routes, RouterModule} from '@angular/router';
 
 const appRoutes: Routes = [
   {path: '', redirectTo: '/recipes', pathMatch: 'full'},
-  {path: 'recipes', loadChildren: './recipes/recipe.module#RecipeModule'}
+  {path: 'recipes', loadChildren: './recipes/recipe.module#RecipeModule'},
+  {path: 'shopping-list', loadChildren: './shopping-list/shopping-list.module#ShoppingListModule'},
+  {path: 'auth', loadChildren: './auth/auth.module#AuthModule'}
 
   // below one is new one for above line
   /*path: 'recipes', loadChildren: () => import('./recipes/recipe.module').then(m => {
